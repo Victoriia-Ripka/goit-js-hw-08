b4c3806e6304b559e53119932b00fdb52e974562
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-08-12 20:27:03 +0300

    hw07

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "SimpleLightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+const cardsMarkUp = createCardsMarkUp(galleryItems);
+
+galleryDiv.insertAdjacentHTML('beforeend', cardsMarkUp);
+galleryDiv.addEventListener('click', onClick);
+
+function createCardsMarkUp(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`}).join('');
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    let gallery = new SimpleLightbox('.gallery__item', {
+        captionDelay: 250,
+        captionsData: "alt",
+    });
+    gallery.on('show.simplelightbox');
+}
