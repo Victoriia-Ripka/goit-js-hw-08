47e0d62657b1598b45e4875755661f78c3148156
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-08-13 19:18:53 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let videoTime;
+
+if(localStorage.getItem('videoplayer-current-time')){
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+player.on('timeupdate', throttle((data) => {
+    videoTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', Number(data.seconds));
+}, 1000))
