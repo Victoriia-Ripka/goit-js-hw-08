b61da7896b7d8941cf5694929356c28529057b1d
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-08-16 09:02:13 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,13 @@ import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-let videoTime;
+const savedCurrentTime = localStorage.getItem('videoplayer-current-time');
 
-if(localStorage.getItem('videoplayer-current-time')){
-    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+if(savedCurrentTime){
+    player.setCurrentTime(savedCurrentTime);
 }
 
 player.on('timeupdate', throttle((data) => {
-    videoTime = data.seconds;
-    localStorage.setItem('videoplayer-current-time', Number(data.seconds));
+    const {seconds} = data;
+    localStorage.setItem('videoplayer-current-time', Number(seconds));
 }, 1000))
